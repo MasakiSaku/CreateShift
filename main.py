fbc55727ecdf9a1cc8939f3b7be1ab8fddb8b7a8
Masaki Sakugawa <e175728@ie.u-ryukyu.ac.jp>
2019-08-28 13:40:49 +0900

    add PersonClass

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("hello world")
+class Person(object):
+    def __init__(self,no,name,time,TakeOff)
+        self.no = no
+        self.name = name
+        #出勤の時間帯、0が朝、１が昼、２が夜
+        self.time = time
+        #希望休
+        self.TakeOff = TakeOff
+
+
