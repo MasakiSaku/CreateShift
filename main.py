8f63e42d3a9f887e2824877ecbf1be17b9b0ce00
Masaki Sakugawa <e175728@ie.u-ryukyu.ac.jp>
2019-09-15 17:31:09 +0900

    create OneMax

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import random
+
+from deap import base
+from deap import creator
+from deap import tools
+
 class Person(object):
     def __init__(self,no,name,time,TakeOff):
         #従業員の番号、あんまり必要ないかも？
@@ -15,6 +21,29 @@ class Person(object):
         return self.time
 
 
+#適応度の適宜
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+#個体の定義
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+#遺伝子を作成する関数
+toolbox.register("attr_bool", random.randint, 0, 1)
+#個体を作成する関数
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
+#世代を生成する関数
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+#評価関数
+def evalOneMax(individual):
+    return sum(individual),
+
+toolbox.register("evaluate", evalOneMax)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+
 def main():
     #従業員の情報
     e0 = Person(0,"mg",[0,1],[23,30,20])
@@ -26,7 +55,75 @@ def main():
 
     employees = [e0,e1,e2,e3,e4,e5]
 
-    print(employees[5].get_TakeOff())
+    #random.seed(64)
+
+    #300個体生成
+    pop = toolbox.population(n=300)
+
+    #交叉率、突然変異率
+    CXPB, MUTPB = 0.5, 0.2
+
+    print("Start of evolution")
+
+    #初期世代の個体の適応度
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    print("  Evaluated %i individuals" % len(pop))
+
+    #全ての個体の適応度のリスト
+    fits = [ind.fitness.values[0] for ind in pop]
+
+    g = 0
+
+    while max(fits) < 100 and g < 1000:
+        g = g + 1
+        print("-- Generation %i --" % g)
+
+        offspring = toolbox.select(pop, len(pop))
+        offspring = list(map(toolbox.clone, offspring))
+
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        print("  Evaluated %i individuals" % len(invalid_ind))
+
+        pop[:] = offspring
+
+        fits = [ind.fitness.values[0] for ind in pop]
+
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2 / length - mean**2)**0.5
+
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+
+    print("-- End of (successful) evolution --")
+
+    best_ind = tools.selBest(pop, 1)[0]
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
 
 if __name__ == '__main__':
     main()
